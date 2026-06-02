56ce64761536297e109ddb0456abea79f2b4cfe1
Louis Liu <liulouis1@gmail.com>
2026-06-02 16:10:26 +0100

    loading datasets

--- SQL/mongodb/D2W2MainTask.py
+++ SQL/mongodb/D2W2MainTask.py
@@ -4,20 +4,49 @@ import pymongo
 client = pymongo.MongoClient()
 db = client['StarWars']
 
-class Starships:
-    def __init__():
-        starships = db['StarShips']
 
-    # get pilot ids from mongodb
-    def get_pilot_id():
-        return db.starships.distinct('pilot_id')
+def fetch_swapi_data():
+    """ Fetches data from swapi """
+    url = f"https://swapi.info/api/starships"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+def save_starships():
+    starships_raw = fetch_swapi_data()
+    if starships_raw:
+            collection = db['starships']
+            collection.insert_many(starships_raw)
+            print("Successfully saved starships")
+    else:
+        print("Failed to save starships")
+
+
+
+class Starships:
+    def __init__(self):
+        self.starships = db['starships']
+
+    def get_pilot_id(self):
+        """ Getter for pilot id """
+        return db.self.starships.distinct('pilot_id')
 
     # replace pilots and insert ids
-    def replace_pilots():
-        pass
+    def replace_pilots(self):
+        """ Replaces pilot ids with new ids """
+        db.self.starships.find_one_and_replace(
+            {'pilot_id': {'$in': self.get_pilot_id()}}, )
 
     # add transformed data to your mongo database
-    def add_transfomred_data():
+    def add_transfomred_data(self):
+        """ Adds new data to database """
         pass
 
 
@@ -27,5 +56,5 @@ class Starships:
 # - Do the same for another API (e.g. pokemon api or similar)
 
 if __name__ == '__main__':
-    pilot_ids = get_pilot_id()
-    print(pilot_ids)
+    test = Starships()
+    test.get_pilot_id()
